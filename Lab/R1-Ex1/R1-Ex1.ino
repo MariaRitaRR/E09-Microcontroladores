7186a9ec9f3f2f63320727880adc36b165a95b2a
MariaRitaRR <mariaritaraposo@outlook.com>
2025-02-20 14:16:51 -0300

    R1

---
+++ Lab/R1-Ex1/R1-Ex1.ino
@@ -0,0 +1,34 @@
+int main(void)
+{
+  DDRD = DDRD | 0b10000000; //Pino PD7 definido como saída
+  PORTD = PORTD | 0b00100000; // Habilitar PULL-UP no PD5
+  PORTD = PORTD & ~(0b10000000); //Desliga a saída PD7
+
+
+  DDRD = DDRD | 0b01000000; //Pino PD6 definido como saída
+  PORTD = PORTD | 0b00010000; // Habilitar PULL-UP no PD4
+  PORTD = PORTD & ~(0b01000000); //Desliga a saída PD6
+
+
+  for (;;)//Super Lopp
+  {
+    int botao1 = PIND & 0b00100000; //Le o estado de PD5
+    int botao2 = PIND & 0b00010000; //Le o estado de PD4
+    
+    if (botao1 == 0)//Botão esta pressionado?
+    {
+      PORTD = PORTD | 0b10000000; //PD7 -> High
+      _delay_ms(1000);
+      PORTD = PORTD & ~(0b10000000); ///PD7 -> LOW
+      
+      }
+    if (botao2 == 0)//Botão esta pressionado?
+    {
+      PORTD = PORTD | 0b01000000; //PD6 -> High
+      _delay_ms(1000);
+      PORTD = PORTD & ~(0b01000000); ///PD7 -> LOW
+      
+      }
+    
+    }
+}
